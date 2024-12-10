44a7455d91b5f9aa640f2d26180112d712713b9e
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-10 15:44:54 +0330

    create class based views for index and post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
+
+
+def index_view(request):
+    return render(request, "index.html")
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"] = "Saeed"
+        context["posts"] = Post.objects.all()
+        return context
+
+
+class PostList(ListView):
+
+    # queryset = Post.objects.all()
+    # model = Post
+    context_object_name = "posts"
+    paginate_by = 2
+    ordering = ["-id"]
+
+    def get_queryset(self):
+
+        posts = Post.objects.all()
+        return posts
+
+
+class PostDetail(DetailView):
+
+    model = Post
