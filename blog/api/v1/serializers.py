0c9c9f8808b17de85540c532f3f2f87f4256a73c
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-15 13:05:48 +0330

    create a model serializer

--- blog/api/v1/serializers.py
+++ blog/api/v1/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
 
+from blog.models import Post
 
-class PostSerializers(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+
+# class PostSerializers(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+
+
+class PostSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ["id", "title", "content", "status", "created_date", "published_date"]
