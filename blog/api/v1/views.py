bfe86d17d6cf20919c258f2e0185fc4ceab2b368
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-15 12:50:21 +0330

    create api view for post detail

--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -1,7 +1,24 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import PostSerializers
+from blog.models import Post
+from rest_framework import status
 
 
 @api_view(["GET"])
 def api_post_list(request):
-    return Response("Hello")
+    posts = Post.objects.filter(status=True)
+    serializer = PostSerializers(posts, many=True)
+    return Response(serializer.data)
+
+
+@api_view()
+def api_post_detail(request, id):
+
+    try:
+        post = Post.objects.get(pk=id)
+        serializer = PostSerializers(post)
+
+        return Response(serializer.data)
+    except Post.DoesNotExist:
+        return Response("Not found", status=status.HTTP_404_NOT_FOUND)
