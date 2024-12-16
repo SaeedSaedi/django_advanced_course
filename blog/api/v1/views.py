180a665ad9566545397934ff187cc5b236a95484
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-16 12:39:28 +0330

    add permission classes decorator and is authenticated class

--- blog/api/v1/views.py
+++ blog/api/v1/views.py
@@ -1,13 +1,14 @@
-import re
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .serializers import PostSerializers
 from blog.models import Post
 from rest_framework import status
 
 
 @api_view(["GET", "POST"])
+@permission_classes([IsAuthenticated])
 def api_post_list(request):
     if request.method == "GET":
         posts = Post.objects.filter(status=True)
