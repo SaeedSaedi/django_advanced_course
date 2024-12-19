bf833eec01322481b9232a2b2e395301dc97e662
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-19 11:12:11 +0330

    use default router to create route

--- blog/api/v1/urls.py
+++ blog/api/v1/urls.py
@@ -1,21 +1,27 @@
 from django.urls import include, path
 import blog.api.v1.views as views
+from rest_framework.routers import DefaultRouter
 
 app_name = "api_v1"
 
-urlpatterns = [
-    # path("post/", views.api_post_list, name="post-list"),
-    # path("post/", views.PostList.as_view(), name="post-list"),
-    # path("post/<int:id>/", views.api_post_detail, name="post-detail"),
-    # path("post/<int:id>/", views.PostDetail.as_view(), name="post-detail"),
-    path(
-        "post/",
-        views.PostViewSet.as_view({"get": "list", "post": "create"}),
-        name="post-list",
-    ),
-    path(
-        "post/<int:id>/",
-        views.PostViewSet.as_view({"get": "retrieve"}),
-        name="post-list",
-    ),
-]
+router = DefaultRouter()
+router.register("post", views.PostViewSet, "post")
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     # path("post/", views.api_post_list, name="post-list"),
+#     # path("post/", views.PostList.as_view(), name="post-list"),
+#     # path("post/<int:id>/", views.api_post_detail, name="post-detail"),
+#     # path("post/<int:id>/", views.PostDetail.as_view(), name="post-detail"),
+#     path(
+#         "post/",
+#         views.PostViewSet.as_view({"get": "list", "post": "create"}),
+#         name="post-list",
+#     ),
+#     path(
+#         "post/<int:id>/",
+#         views.PostViewSet.as_view({"get": "retrieve"}),
+#         name="post-list",
+#     ),
+# ]
