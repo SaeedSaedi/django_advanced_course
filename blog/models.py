7a0f01a5851e28a3410d0b5efe2b5c1d2f561ea3
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-19 13:31:34 +0330

    create get_snippet and get_absolute_api_url function for Post Model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 User = get_user_model()
 
@@ -22,6 +23,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_snippet(self):
+        return self.content[0:5]
+
+    def get_absolute_api_url(self):
+        return reverse("blog:api_v1:post-detail", kwargs={"id": self.pk})
 
 
 class Category(models.Model):
