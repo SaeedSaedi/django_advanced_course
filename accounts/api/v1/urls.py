7979621ae2ca8a5ee8f665df9e1b2aac910e22ea
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-22 14:08:41 +0330

    add login url

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
 from . import views
+from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name = "api_v1"
 
 urlpatterns = [
-    path("registration/", views.RegistrationApiView.as_view(), name="registration")
+    path("registration/", views.RegistrationApiView.as_view(), name="registration"),
+    path("token/login/", ObtainAuthToken.as_view(), name="token-login"),
 ]
