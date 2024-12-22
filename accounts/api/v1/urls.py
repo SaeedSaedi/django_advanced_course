3af0e7ff07fa18cff4e0a6ae10c01f060ba29429
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-22 14:26:30 +0330

    create url for custom obtain auth token

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from . import views
-from rest_framework.authtoken.views import ObtainAuthToken
+# from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name = "api_v1"
 
 urlpatterns = [
     path("registration/", views.RegistrationApiView.as_view(), name="registration"),
-    path("token/login/", ObtainAuthToken.as_view(), name="token-login"),
+    # path("token/login/", ObtainAuthToken.as_view(), name="token-login"),
+    path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
 ]
