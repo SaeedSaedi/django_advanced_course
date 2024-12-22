f5385e8bc6d90c4165cb325afbe385e5fc5c55e2
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-22 16:27:49 +0330

    create simple jwt urls

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -1,5 +1,11 @@
 from django.urls import include, path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 
 # from rest_framework.authtoken.views import ObtainAuthToken
 
@@ -10,4 +16,7 @@ urlpatterns = [
     # path("token/login/", ObtainAuthToken.as_view(), name="token-login"),
     path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
     path("token/logout/", views.CustomDiscardAuthToken.as_view(), name="token-logout"),
+    path("jwt/create/", TokenObtainPairView.as_view(), name="jwt-create"),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify'),
 ]
