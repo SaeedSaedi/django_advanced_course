890647f92625cab1cbfff129a222438ff324f85d
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-22 11:34:42 +0330

    add accounts urls

--- core/urls.py
+++ core/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("blog/", include("blog.urls")),
     path("api-auth/", include("rest_framework.urls")),
+    path("accounts/", include("accounts.urls")),
     path("api-docs/", include_docs_urls(title="api-sample")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     # Optional UI:
