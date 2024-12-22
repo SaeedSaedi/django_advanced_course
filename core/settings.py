59dc0d967b1eb1c4378c358f9dbeabcb1e62f72d
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-22 12:23:34 +0330

    add authtoken to install app

--- core/settings.py
+++ core/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = config(
 
 INSTALLED_APPS = [
     "rest_framework",
+    "rest_framework.authtoken",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
