05bc27d961e0445b777b79bd3e2e87799cdea53a
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-22 15:47:52 +0330

    create url for logout

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from . import views
+
 # from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name = "api_v1"
@@ -8,4 +9,5 @@ urlpatterns = [
     path("registration/", views.RegistrationApiView.as_view(), name="registration"),
     # path("token/login/", ObtainAuthToken.as_view(), name="token-login"),
     path("token/login/", views.CustomObtainAuthToken.as_view(), name="token-login"),
+    path("token/logout/", views.CustomDiscardAuthToken.as_view(), name="token-logout"),
 ]
