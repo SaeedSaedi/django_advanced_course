ff73e8bcfcdc3b47877db35c5a6136f4b221e847
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-23 11:47:13 +0330

    create generic view for change password

--- accounts/api/v1/views.py
+++ accounts/api/v1/views.py
@@ -3,9 +3,16 @@ from rest_framework.authtoken.models import Token
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import RegistrationSerializer, CustomAuthTokenSerializer
+from .serializers import (
+    RegistrationSerializer,
+    CustomAuthTokenSerializer,
+    ChangePasswordSerializer,
+)
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class RegistrationApiView(generics.GenericAPIView):
@@ -40,3 +47,32 @@ class CustomDiscardAuthToken(APIView):
     def post(self, request):
         request.user.auth_token.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ChangePasswordApiView(generics.GenericAPIView):
+    model = User
+    permission_classes = [IsAuthenticated]
+    serializer_class = ChangePasswordSerializer
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+
+        return obj
+
+    def put(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            if not self.object.check_password(serializer.data.get("old_password")):
+                return Response(
+                    {"old_password": "wrong password"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            return Response(
+                {"details": "password changed successfully"}, status=status.HTTP_200_OK
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
