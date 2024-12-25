89869deebdf637d92ae54ad2f5ae917abbc1eb90
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-25 11:29:33 +0330

    create fake user and get profile

--- blog/management/commands/insert_data.py
+++ blog/management/commands/insert_data.py
@@ -1,10 +1,18 @@
 from django.core.management.base import BaseCommand
 from faker import Faker
+from accounts.models import User, Profile
+from blog.models import Post, Category
 
 
 class Command(BaseCommand):
     help = "insert data"
 
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.fake = Faker()
+
     def handle(self, *args, **options):
-        fake = Faker()
-        print(fake.name())
+        user = User.objects.create_user(email=self.fake.email(), password="Test@123456")
+        profile = Profile.objects.get(user=user)
+        print(user)
+        print(profile)
