34a4098e8f1eb168022935a6ff42666e4f95872a
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-25 16:11:44 +0330

    get token with locust

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -3,6 +3,16 @@ from locust import HttpUser, task
 
 class HelloWorldUser(HttpUser):
 
+    def on_start(self):
+        response = self.client.post(
+            "/accounts/api/v1/jwt/create/",
+            data={"email": "saeed@saeed.com", "password": "test"},
+        ).json()
+        self.client.headers = {
+            "Authorization": f"Bearer {response.get('access', None)}"
+        }
+        return super().on_start()
+
     @task
     def post_list(self):
         self.client.get("/blog/api/v1/post/")
