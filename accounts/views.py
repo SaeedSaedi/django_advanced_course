e7cdaabbf467bd802ee61ef31c9ddad0d5b6ef9e
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-26 15:46:20 +0330

    use cache_page decorator

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 from .tasks import sendEmail
 import requests
 
@@ -11,10 +12,12 @@ def send_mail(request):
     return HttpResponse("email sent")
 
 
+@cache_page(60)
 def test(request):
-    if not cache.get("test_delay"):
-        response = requests.get(
-            "https://f131de9d-a934-4741-a533-c2b9332eee16.mock.pstmn.io/test/5"
-        )
-        cache.set("test_delay", response.json(), 30)
-    return JsonResponse(cache.get("test_delay"))
+    # if not cache.get("test_delay"):
+    response = requests.get(
+        "https://f131de9d-a934-4741-a533-c2b9332eee16.mock.pstmn.io/test/5"
+    )
+    # cache.set("test_delay", response.json(), 30)
+    # return JsonResponse(cache.get("test_delay"))
+    return JsonResponse(response.json())
